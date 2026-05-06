d75afbcb555b3531ec1a347f01679a07734ad5e9
Sergio M. <50914599+sergiomb04@users.noreply.github.com>
2026-05-06 19:05:27 +0000

    create next app: first version (hello world)

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="m-auto text-center justify-center items-center h-vh">
+      <h1 className="w-max">Hola Mundo! 👋</h1>
+    </main>
+  );
+}
